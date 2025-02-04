b0fe0a1ed7bbfc4144cb417fb65603284dcd0966
sukirth-singh-gaur <142683858+sukirth-singh-gaur@users.noreply.github.com>
2025-02-04 18:56:38 +0000

    datatype conversions

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = true
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33acd" => nan
+// true => 1
+
+let isLoggedIn = 1
+
+let booleanConv = Boolean(isLoggedIn)
+console.log(booleanConv);
+
+// 1 => true
+//"" => false
+//"she" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+

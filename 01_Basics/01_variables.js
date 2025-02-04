039126da75c21342eb3d40a87c8ae1b67aa82a6d
sukirth-singh-gaur <142683858+sukirth-singh-gaur@users.noreply.github.com>
2025-02-04 18:47:39 +0000

    datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553;
+let accountEmail = "Sukirthsingh@gmail.com";
+var accountPassword = "sukki";
+accountCity = "Gurugram";
+let accountState;
+
+//accountId = 2; // not allowed 
+
+accountEmail = "lulu@los";
+accountPassword = "2121221";
+accountCity = "Bengal";
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState]);
---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3 + 3) // we are using node js not browser
+
+console.log(3 + 3)
+
+let name = "Sukirth"
+let age = 21
+let isloggedIn = false
+
+//datatypes 
+//number => 2 to power 53
+//big int
+//string = ""
+//boolean = t/f
+//null => standalone value
+//undefined => not assigned value
+//symbol => unique
+
+//object 
+console.log(typeof undefined);//->undefined
+
+
+console.log(typeof null); //-> object
